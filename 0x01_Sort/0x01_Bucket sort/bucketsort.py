114a56db999cdbba454176bb008570d0a8f374b0
yb123speed <904896209@qq.com>
2019-01-12 11:01:49 +0800

    Python Code implementation

--- 0x01_Sort/0x01_Bucket sort/bucketsort.py
+++ 0x01_Sort/0x01_Bucket sort/bucketsort.py
@@ -1,5 +1,15 @@
 # _*_ code:utf-8 _*_
 #!/usr/local/bin/python
 
+buckets = [0 for i in range(11)]
+print (buckets)
 
+for i in range(5):
+    score = int(input("请输入成绩:"))
+    buckets[score] +=1
 
+for i in range(10,-1,-1):
+    if buckets[i]>0:
+        for j in range(buckets[i]):
+            print(i)
+        
