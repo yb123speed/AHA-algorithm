93e1e983c2b9cdc2333e080968c197623fc87362
yb123speed <904896209@qq.com>
2019-01-12 16:58:55 +0800

    快速排序的python实现

---
+++ 0x01_Sort/0x03_Quick sort/quicksort.py
@@ -0,0 +1,31 @@
+# _*_ code:utf-8 _*_
+#!/usr/local/bin/python
+
+scores = []
+while 1:
+    score = input("请输入成绩，输入q结束输入：")
+    if score == 'q':
+        break
+    scores.append(int(score))
+
+def quicksort(left, right):
+    if left>right:
+        return
+    temp = scores[left]
+    i, j = left, right
+    while i!=j:
+        while scores[j]>=temp and i<j:
+            j-=1
+        while scores[i]<=temp and i<j:
+            i+=1
+        
+        if i<j:
+            scores[i], scores[j] = scores[j], scores[i]
+    
+    scores[left] = scores[i]
+    scores[i] = temp
+    quicksort(left,i-1)
+    quicksort(i+1,right)
+
+quicksort(0, len(scores)-1)
+print(scores)
